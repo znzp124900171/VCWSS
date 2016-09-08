1ec94fa9d1544a191791971b96c2ed96eddca3ea
znzp124900171 <znzp124900171@gmail.com>
2016-09-08 03:37:15 +0200

    改动

--- root/src/js/vcwss-intro.js
+++ root/src/js/vcwss-intro.js
@@ -2,4 +2,20 @@ function activaTab(tab){
     $('.nav-tabs a[href="#' + tab + '"]').tab('show');
 };
 
+$(".team-pic-small").click(function(event) {
+	var offset = $(event.target).position();
+	var path = $(this).src;
+	var name = $(this).attr("alt");
+	var title = $(this).data('title');
+
+	$(".team-pic-detail h4").html('<h4>'+name+'&nbsp<small>'+title+'</small></h4>')
+	$(".team-pic-detail").css({
+		top: offset.bottom,
+		left: offset.left-15+"px"
+	});
+	$(".team-pic-detail img").attr('src', path);
+	$(".team-pic-detail").toggle("slow");
+
+	console.log(name);
+});
 activaTab('vcwss-team');
