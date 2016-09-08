db10c47773b7ddd6bf07b52d227ea920647b3243
znzp124900171 <znzp124900171@gmail.com>
2016-09-09 00:58:28 +0200

    gaidong

--- root/src/js/vcwss-intro.js
+++ root/src/js/vcwss-intro.js
@@ -3,7 +3,7 @@ function activaTab(tab){
 };
 
 $(".team-pic-small").click(function(event) {
-	var offset = $(event.target).position();
+	var offset = $(event.target).offset();
 	var path = $(this).src;
 	var name = $(this).attr("alt");
 	var title = $(this).data('title');
@@ -11,7 +11,7 @@ $(".team-pic-small").click(function(event) {
 	$(".team-pic-detail h4").html('<h4>'+name+'&nbsp<small>'+title+'</small></h4>')
 	$(".team-pic-detail").css({
 		top: offset.bottom,
-		left: offset.left-15+"px"
+		left: offset.left
 	});
 	$(".team-pic-detail img").attr('src', path);
 	$(".team-pic-detail").toggle("slow");
