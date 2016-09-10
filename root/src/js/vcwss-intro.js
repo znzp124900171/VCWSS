e137d703ad29ceabbad4eb8f4cc6c7cf2fe5ad2a
znzp124900171 <znzp124900171@gmail.com>
2016-09-11 01:28:06 +0200

    添加了图片

--- root/src/js/vcwss-intro.js
+++ root/src/js/vcwss-intro.js
@@ -8,11 +8,11 @@ $('[data-toggle="popover"]').popover({
 	trigger: 'click',
 	content: function(){
 		var name = $(this).data().name;
-		var content_wrapper = ['<img src="../../img/team-1.jpg">','<h5>',name,'</h5>'].join('');
+		var path = $(this).find('.team-pic-small').attr('src');
+		var content_wrapper = ['<img src="',path,'" alt="members"><h5>',name,'</h5>'].join('');
 		return content_wrapper;
 	}
 });
 
 
-console.log($('[data-toggle="popover"]').data().name);
 activaTab('vcwss-team');
