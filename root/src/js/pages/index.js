a1ac3b6aa86f60cca6a58cfbdd72244ac6020afe
znzp124900171 <znzp124900171@gmail.com>
2016-09-19 22:04:05 +0200

    修改引用路径和一些布局调整，可以发布静态页面

--- root/src/js/pages/index.js
+++ root/src/js/pages/index.js
@@ -1,4 +1,4 @@
-var urlList = ['../img/homepage/event3.jpg','../img/homepage/event2.jpg','../img/homepage/event3.jpg','../img/homepage/event4.jpg']; //已经解析过的jsons
+var urlList = ['../img/homepage/event1.jpg','../img/homepage/event2.jpg','../img/homepage/event3.jpg','../img/homepage/event4.jpg']; //已经解析过的jsons
 
 $('.event-block-list .img-wrapper img').each(function(index) {
 	$(this).attr('src', urlList[index]);
