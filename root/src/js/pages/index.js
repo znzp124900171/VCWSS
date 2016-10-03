cb3b96c045796e63042fe658ae1bf56ac82fbb6e
znzp124900171 <znzp124900171@gmail.com>
2016-10-03 17:14:58 +0200

    首页JS更新，匹配接口，index调整

--- root/src/js/pages/index.js
+++ root/src/js/pages/index.js
@@ -1,5 +1,76 @@
-var urlList = ['../img/homepage/event1.jpg','../img/homepage/event2.jpg','../img/homepage/event3.jpg','../img/homepage/event4.jpg']; //已经解析过的jsons
+var sysLan = window.navigator.language;
 
-$('.event-block-list .img-wrapper img').each(function(index) {
-	$(this).attr('src', urlList[index]);
+$('#lang-cn').click(function() {
+	sysLan = 'zh-CN';
 });
+
+$('#lang-de').click(function() {
+	sysLan = 'de-DE';
+});
+
+if(sysLan === 'zh-CN') {
+	// 轮播框
+	$.getJSON('http://47.90.202.48:8080/vcwss/f/carouselContents?lang=0', function(carousel) {
+		if (carousel.success) {
+			$.each(carousel.data.list, function(i,list){
+				var id = '#carousel-'+i;
+				$(id+' .carousel-title').text(list.title);
+				$(id+' .carousel-text p').text(list.subtitle);
+				$(id+' .carousel-text a').attr('href', list.url);
+				$(id+' img').attr('src', list.img);
+			});
+		}
+	});
+
+	// 近期活动
+	$.getJSON('http://47.90.202.48:8080/vcwss/f/recentEvents?lang=0', function(recentEvents){
+		if(recentEvents.success) {
+			$.each(recentEvents.data.list, function(i, list) {
+				var id = '#event-'+i;
+				$(id+' .title-block a').text(list.title);
+				$(id+' a').attr('href', list.url);
+				$(id+' img').attr('src', list.img);
+				$(id+' .time-block').text(list.eventTime);
+				$(id+' .location-block').text(list.eventAddress);
+			});
+		};
+	});
+
+	// 近期新闻
+	$.getJSON('http://47.90.202.48:8080/vcwss/f/recentNews?lang=0', function(recentNews) {
+		if(recentNews.success) {
+			$.each(recentNews.data.list, function(i, list) {
+				var id = '#news-'+i;
+				$(id+' .title-block a').text(list.title);
+				$(id+' a').attr('href', list.url);
+				$(id+' img').attr('src', list.img);
+				$(id+' .time-block-news').text(list.releaseTime);
+			});
+		};
+	});
+
+	// 广告框
+	$.getJSON('http://47.90.202.48:8080/vcwss/f/ads/?lang=0', function(ads) {
+		if(ads.success) {
+			$.each(ads.data.list, function(i,list) {
+				$('.parallax .heading h2').text(list.title);
+				$('.parallax .subheading h4').text(list.subtitle);
+				$('#inner-section').css('background', 'url('+list.img+') center no-repeat');
+			});
+		}
+	});
+
+	// 画廊
+	$.getJSON('http://47.90.202.48:8080/vcwss/f/galleries?lang=0', function(galleries) {
+		if (galleries.success) {
+			$.each(galleries.data.list, function(i,list){
+				var id = '#gallery-'+i;
+				$(id+' .work-desc a').text(list.thema);
+				$(id+' .img').attr('src', list.coverImg);
+				$(id+' .item-img-overlay a').attr('href', list.coverImg);
+				$(id+' .work-desc a').attr('href', list.url);
+			});
+		}
+	});
+};
+
